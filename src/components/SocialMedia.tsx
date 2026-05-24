3dbbd50f7e55a66df43ae0cf2ba3e7e189b7e462
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2026-05-24 18:02:47 +0200

    social update

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -6,7 +6,6 @@ export function SocialRow(){
             <div className="row align-items-center">
                 <div className="col-2 center"><a href="https://www.reddit.com/user/hurly_burly_pegasus/?rdt=59692"  property="url"><i className="fa fa-reddit"/></a></div>
                 <div className="col-2 center"><a href="mailto:lilisisopods@proton.me" property="url"><i className="fa fa-envelope"/></a></div>
-                <div className="col-2 center"><a href="https://www.instagram.com/lilisisopods/" property="email"><i className="fa fa-instagram"/></a></div>
                 {/* <div className="col-2 center"><a href="#" className="fa fa-github" property="email"></a></div> */}
             </div>
         </div>
